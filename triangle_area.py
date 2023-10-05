475c1097a1dcf487a320c1cdd43552dbd548d0de
SSFSKIM <kimmi@ssfs.org>
2023-10-06 00:25:20 +0900

    dfdas

--- triangle_area.py
+++ triangle_area.py
@@ -1,2 +1,12 @@
+#Steve
+#10.05.23
 
+import math
+a = int(input('what\' one side '))
+b = int(input('what\'s the other side?'))
+c = int(input('what\s the last side?'))
 
+def tr_a(x, y, z):
+    s = (x+y+z)/2
+    print(math.sqrt(s*(s-a)*(s-b)*(s-c)))
+tr_a(a, b, c)
