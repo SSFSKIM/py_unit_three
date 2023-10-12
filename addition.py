b7e4295c22d09ea9d6cacdb25c55c5adc7badb28
SSFSKIM <kimmi@ssfs.org>
2023-10-12 11:57:38 -0400

    ty

--- addition.py
+++ addition.py
@@ -1,9 +1,19 @@
+#Steve
+#10.11
+
+
+c = int(input())
+d = int(input())
 # Write your addition function in the space below...
 
+def add(a, b):
+    print(f'The sum of {a} and {b} is {a+b}')
 
 # Do not change anything below these lines
 def main():
     pass
 
 if __name__ == '__main__':
+    add(c, d)
+
     main()
