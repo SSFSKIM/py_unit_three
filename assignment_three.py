60ffe5435aa8828bcfada3edbcadb3347ec72ed2
SSFSKIM <kimmi@ssfs.org>
2023-10-19 12:17:02 +0900

    I resubmit due to missing docstrings

--- assignment_three.py
+++ assignment_three.py
@@ -2,23 +2,31 @@
 #10.17.23
 
 import turtle
-
+#get the input of informations: length, colors
 lgth = float(input('whaat is length'))
 sd = input('what\'s the color')
 ct = input('what\'s the color')
 
 
-def hex():
+def hex(cl, lg):
+    '''
+    function that draws central hexagon. take color and length as parameter to draw hexagon
+    :return:
+    '''
     turtle.begin_fill()
-    turtle.fillcolor(ct)
+    turtle.fillcolor(cl)
     for a in range(6):
-        turtle.forward(lgth)
+        turtle.forward(lg)
         turtle.right(60)
     turtle.end_fill()
-def side_hex():
+def side_hex(cl):
+    '''
+    draws peripheral hexagons. take color as parameter and draw hexagon
+    :return:
+    '''
     for a in range(6):
         turtle.begin_fill()
-        turtle.fillcolor(sd)
+        turtle.fillcolor(cl)
         for a in range(6):
             turtle.forward(lgth)
             turtle.left(60)
@@ -26,9 +34,13 @@ def side_hex():
         turtle.right(60)
         turtle.end_fill()
 def draw_hex():
+    '''
+    draw full hexagon using two hexagon functions. no parameter, draw full hexagon
+    :return:
+    '''
     turtle.left(60)
-    hex()
-    side_hex()
+    hex(ct, lgth)
+    side_hex(sd)
 
 
 draw_hex()
